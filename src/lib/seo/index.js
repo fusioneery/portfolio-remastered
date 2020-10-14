282589f300a1c2a93423482f54f42c991fa639b0
Vlad Abramov <neoandmatrix2@gmail.com>
2020-10-14 12:09:23 +0300

    bugfix: meta name="image" corrected

--- src/lib/seo/index.js
+++ src/lib/seo/index.js
@@ -30,7 +30,7 @@ export const SEO = ({
     >
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="image" content={image} />
+      <meta name="image" content={metaImage} />
       <meta name="description" content={metaDescription} />
       <meta name="author" content="Vlad Abramov" />
 
